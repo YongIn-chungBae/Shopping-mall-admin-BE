bcb2dfb8e449e65dfaa30cac1f82a3260e75910d
Chung Beom Cho <cndqjacndqja@gmail.com>
2021-09-04 18:39:59 +0900

    init

---
+++ src/main/java/com/example/shoppingmalladminbe/ShoppingMallAdminBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingmalladminbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallAdminBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallAdminBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shoppingmalladminbe/ShoppingMallAdminBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingmalladminbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallAdminBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
